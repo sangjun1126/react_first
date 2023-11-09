7b3d530109dacb8de1599535571755307b612688
sangjun1126 <jeongsangjun1234@gmail.com>
2023-11-09 21:22:26 +0900

    hello

---
+++ src/Counter.js
@@ -0,0 +1,35 @@
+import { Component } from 'react';
+
+class Counter extends Component {
+  state = {
+    number: 0,
+    fixedNumber: 0,
+  };
+  render() {
+    const { number, fixedNumber } = this.state; // state를 조회할 떄는 this.state로 조회합니다.
+    return (
+      <div>
+        <h1>{number}</h1>
+        <h2>바뀌지 않는 값: {fixedNumber}</h2>
+        <button
+          // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정합니다.
+          onClick={() => {
+            this.setState(
+              {
+                number: number + 1,
+              },
+              () => {
+                console.log('방금 state가 호출되었습니다.');
+                console.log(this.state);
+              }
+            );
+          }}
+        >
+          +1
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
