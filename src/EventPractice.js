0711715677c335c630af1b30228a7fb9e4c8e2e7
sangjun1126 <jeongsangjun1234@gmail.com>
2023-11-10 10:13:29 +0900

    first commit

---
+++ src/EventPractice.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+const EventPractice = () => {
+  const [username, setUsername] = useState('');
+  const [message, setMessage] = useState('');
+  const onChangeUsername = (e) => setUsername(e.target.value);
+  const onChangeMessage = (e) => setMessage(e.target.value);
+  const onClick = () => {
+    alert(username + ': ' + message);
+    setUsername('');
+    setMessage('');
+  };
+  const onKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      onClick();
+    }
+  };
+  return (
+    <div>
+      <h1>이벤트 연습</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="사용자명"
+        value={username}
+        onChange={onChangeUsername}
+      />
+      <input
+        type="text"
+        name="message"
+        placeholder="아무거나 입력해보세요"
+        value={message}
+        onChange={onChangeMessage}
+        onKeyPress={onkeypress}
+      />
+      <button onClick={onClick}>확인</button>
+    </div>
+  );
+};
+export default EventPractice;
